4cbfcc5ccfe785dcf0207a5378d441423612f195
burakiipekci <burakipekci@outlook.com>
2022-11-07 23:25:09 +0300

    implementations completed

---
+++ Core/DataAccess/EntityFramework/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T Entity);
+        void delete(T Entity);
+        void update(T Entity);
+        T Get(Expression<Func<T, bool>> filter = null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+
+
+    }
+}
--- DataAccess/Concretes/EfOrderDal.cs
+++ DataAccess/Concretes/EfOrderDal.cs
@@ -1,4 +1,6 @@
 ﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstracts;
+using DataAccess.Connection;
 using Entities.Concretes;
 using System;
 using System.Collections.Generic;
@@ -6,9 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstracts
+namespace DataAccess.Concretes
 {
-    public interface IProductDal: IEntityRepository<Product>
+    public class EfOrderDal: EfEntityRepositoryBase<Order, AppDbContext>,IOrderDal
     {
     }
 }
