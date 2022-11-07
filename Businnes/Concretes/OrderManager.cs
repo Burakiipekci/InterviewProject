fe61bdcea21856a601fa885f40fa0f6affcc1684
burakiipekci <burakipekci@outlook.com>
2022-11-07 23:28:20 +0300

    Created

---
+++ Businnes/Concretes/OrderManager.cs
@@ -0,0 +1,13 @@
+﻿using Businnes.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businnes.Concretes
+{
+    public class OrderManager:IOrderService
+    {
+    }
+}
