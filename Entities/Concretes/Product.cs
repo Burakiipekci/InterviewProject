2db0c4dd55390ca27bbec7b7b39513c4de744010
burakiipekci <burakipekci@outlook.com>
2022-11-07 22:27:46 +0300

    I created all the concrete

---
+++ Entities/Concretes/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+        public decimal UnitePrice { get; set; }
+        public int Unit { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
